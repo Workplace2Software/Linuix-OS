29550535d21430e9b26bac904ca3c18dc2afd84a
soafen <soafensoe@protonmail.com>
2022-02-16 16:42:41 -0800

    Update VirusSheild.Designer.vb

--- linuix/VirusSheild.Designer.vb
+++ linuix/VirusSheild.Designer.vb
@@ -77,7 +77,7 @@ Partial Class VirusSheild
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(128, 38)
+        Me.Label1.Location = New System.Drawing.Point(131, 35)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(162, 25)
         Me.Label1.TabIndex = 2
